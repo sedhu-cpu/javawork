fd8e9b15a849b4e360846d664938d0d8433ce144
sedhu-cpu <69859734+sedhu-cpu@users.noreply.github.com>
2021-06-27 15:16:59 +0300

    master

--- inheritance.java
+++ inheritance.java
@@ -1,3 +1,19 @@
-public class inheritance {
-    
+import javax.swing.tree.VariableHeightLayoutCache;
+
+class inheritance {
+    protected String company = "Honda";
+    public void steer(){
+        System.out.println("Tuut, Tuut!");
+    }
+}
+
+class bike extends inheritance{
+    private String model = "idk";
+    public static void main(String[] args) {
+        bike myObj = new bike();
+
+        myObj.steer();
+
+        System.out.println(myObj.company + " " + myObj.model );
+    }
 }
