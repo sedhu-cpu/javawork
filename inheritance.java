b3d14866a4718bb1956b8bef6434f0c1088f8a6b
sedhu-cpu <69859734+sedhu-cpu@users.noreply.github.com>
2021-06-27 15:22:02 +0300

    master

--- inheritance.java
+++ inheritance.java
@@ -1,13 +1,13 @@
 import javax.swing.tree.VariableHeightLayoutCache;
 
-class inheritance {
+class inheritance {//superclass
     protected String company = "Honda";
     public void steer(){
         System.out.println("Tuut, Tuut!");
     }
 }
 
-class bike extends inheritance{
+class bike extends inheritance{// subclass inherting from superclass
     private String model = "idk";
     public static void main(String[] args) {
         bike myObj = new bike();
